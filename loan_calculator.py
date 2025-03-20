98f15096a54ff7b39931baf03f26e780af584443
malcolm-a <aridoryjr@icloud.com>
2025-03-20 21:13:50 +0100

    feat(loan_calculator.py): function to convert result to excel file

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from io import BytesIO
 
 def adjust_rate_and_period(rate, n, period):
 
@@ -67,3 +69,38 @@ def loan_constant_principal(n, amount, rate, period):
     total_interests = round(sum(x[2] for x in result), 2)
     result.append(["Total", "", total_interests, round(amount, 2), round(total_payment, 2)])
     return result
+
+def to_xlsx(data: list, lang: str="en"):
+    import pandas as pd
+    from io import BytesIO
+    
+    if lang == "fr":
+        columns = {
+            0: "Période",
+            1: "Capital dû en début de période",
+            2: "Intérêts",
+            3: "Amortissements",
+            4: "Annuités",
+            5: "Capital dû en fin de période"
+        }
+    else:
+        columns = {
+            0: "Period",
+            1: "Starting Balance",
+            2: "Interest",
+            3: "Principal",
+            4: "Payment",
+            5: "Ending Balance"
+        }
+    
+    df = pd.DataFrame(data)
+    column_mapping = {i: name for i, name in columns.items() if i < len(df.columns)}
+    df.rename(columns=column_mapping, inplace=True)
+    
+    output = BytesIO()
+
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Loan Amortization')
+
+    output.seek(0)
+    return output
